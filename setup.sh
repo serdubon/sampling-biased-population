548949ffbd135368043920a4bee134f57c603241
serdubon <serdubon2162@gmail.com>
2020-08-19 21:16:28 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"serdubon2162@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+\n\
+" > ~/.streamlit/config.toml
